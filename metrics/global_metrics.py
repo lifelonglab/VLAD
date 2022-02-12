ee7f409bacbd8dc8b2852a74c66445078e2d7eed
Kamil Faber <kfaber@agh.edu.pl>
2022-02-12 12:01:33 -0500

    Improve metrics

--- metrics/global_metrics.py
+++ metrics/global_metrics.py
@@ -1,4 +1,4 @@
-from metrics.postprocess.accuracy_global import AccuracyGlobal
+from metrics.postprocess.accuracy_global import MetricGlobal
 from metrics.postprocess.backward_transfer_global import BackwardTransferGlobal
 from metrics.postprocess.forward_transfer_global import ForwardTransferGlobal
 from metrics.tasks_matrix.metrics_matrix_per_task import BaseMetricsMatrixPerTask, BaseMetric, base_metrics
@@ -7,7 +7,7 @@ from metrics.tasks_matrix.metrics_matrix_per_task import BaseMetricsMatrixPerTas
 def calculate_global_metrics(tasks_matrix_metrics: BaseMetricsMatrixPerTask):
     return {
         metric: {
-            'value': (AccuracyGlobal().process(tasks_matrix_metrics.get_single_metric_matrix(metric))),
+            'value': (MetricGlobal().process(tasks_matrix_metrics.get_single_metric_matrix(metric))),
             'backward_transfer': BackwardTransferGlobal().process(
                 tasks_matrix_metrics.get_single_metric_matrix(metric)),
             'forward_transfer': ForwardTransferGlobal().process(tasks_matrix_metrics.get_single_metric_matrix(metric))
--- metrics/postprocess/accuracy_global.py
+++ metrics/postprocess/accuracy_global.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 
-class AccuracyGlobal:
+class MetricGlobal:
     """Following: 'Don’t forget, there is more than forgetting: new metrics for Continual Learning.'"""
     def process(self, input_results: np.ndarray):
         tasks_no = input_results.shape[0]
-        acc_sum = 0
+        val_sum = 0
         for j in range(tasks_no):
             for i in range(j, tasks_no):
-                acc_sum += input_results[i][j]
+                val_sum += input_results[i][j]
 
-        return acc_sum / (tasks_no * (tasks_no + 1) / 2)
+        return val_sum / (tasks_no * (tasks_no + 1) / 2)
+
